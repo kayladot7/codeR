7fd95ddeff18cb0df9fae4900b720f17e88d4d5a
Kayla Dot7 <61982451+kayladot7@users.noreply.github.com>
2020-08-23 11:41:16 +0700

    Update functions.R

--- functions.R
+++ functions.R
@@ -17,3 +17,27 @@ sum_even <- function(start, end){
 drink <- function(price, type="Tea"){
   print(paste("With", price, "USD, you can drink", type,"then."))
 }
+
+electric_cal <- function(n){
+  p1 = 1.678 * n
+  p2 = 1.734 * (n - 50)
+  p3 = 2.014 * (n - 100)
+  p4 = 2.536 * (n - 200)
+  p5 = 2.834 * (n - 300)
+  p6 = 2.927 * (n - 400)
+
+  if(n <= 50){
+    print(paste("Price: ", p1))
+  } else if(n <= 100){
+    print(paste("Price: ", p1 + p2))
+  } else if(n <= 200){
+    print(paste("Price: ", p1 + p2 + p3))
+  } else if(n <= 300){
+    print(paste("Price: ", p1 + p2 + p3 + p4))
+  } else if(n <= 400){
+    print(paste("Price: ", p1 + p2 + p3 + p4 + p5))
+  } else { 
+    print(paste("Price: ", p1 + p2 + p3 + p4 + p5 + p6))
+  }
+  return 
+}
