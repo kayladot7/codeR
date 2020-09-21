c20eee2c3096f9ceb5553ab04cf13ddf0efe6da2
Kayla Dot7 <61982451+kayladot7@users.noreply.github.com>
2020-09-21 14:17:19 +0700

    Update functions.R

--- functions.R
+++ functions.R
@@ -106,3 +106,10 @@ getmode <- function(v){
     uniqv <- unique(v)
     uniqv[which.max(tabulate(match(v, uniqv)))]
 }
+  
+clusters <- function(x, centers){
+  #compute squared euclidean distance from each sample to each cluster center
+  tmp <- sapply(seq_len(nrow(x)),
+  function(i) apply(centers, 1, function(v) sum ((x[i, ]-v)^2)))
+  max.col(-t(tmp)) #find index of min distance
+}
